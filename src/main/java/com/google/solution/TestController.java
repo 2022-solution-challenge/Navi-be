d9b77e843a99823f8b8eb4a72e149dee900fdecc
grace_goose <choieungi@gm.gist.ac.kr>
2022-02-21 13:42:18 +0900

    test: ci test

--- src/main/java/com/google/solution/TestController.java
+++ src/main/java/com/google/solution/TestController.java
@@ -21,6 +21,6 @@ public class TestController {
 
     @GetMapping("/user/{name}")
     public String helloName(@PathVariable String name) {
-        return "Hello! " + name;
+        return "Hello!  " + name;
     }
 }

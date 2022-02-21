a031bfc410ee3ca27b9919c0c7bd6bb715f874a1
grace_goose <choieungi@gm.gist.ac.kr>
2022-02-21 13:39:44 +0900

    test: ci test

---
+++ src/main/java/com/google/solution/TestController.java
@@ -0,0 +1,26 @@
+package com.google.solution;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        return "Hello!, " + request.getRequestURL();
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/user/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello! " + name;
+    }
+}

b4c5bd69ce72b857f3438555bbeabbca5ebecc61
grace_goose <choieungi@gm.gist.ac.kr>
2022-02-23 16:00:32 +0900

    feat: oauth page

--- src/main/java/com/google/solution/user/UserController.java
+++ src/main/java/com/google/solution/user/UserController.java
@@ -10,15 +10,17 @@ public class UserController {
 
     private static final String ENDPOINT = "https://accounts.google.com/o/oauth2/v2/auth";
 
-    @Value("spring.google.oauth.client_id")
+    @Value("${spring.google.oauth.client_id}")
     private String CLIENT_ID ;
-    private static final String REDIRECT_URI = "https://localhost:8080/home";
+    @Value("${spring.google.oauth.redirect_uri}")
+    private String REDIRECT_URI;
     private static final String RESPONSE_TYPE = "code";
     private static final String SCOPE = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
 
 
     @GetMapping("/login")
     public String login() {
+        System.out.println(CLIENT_ID + REDIRECT_URI);
         return "redirect:" + ENDPOINT + "?client_id=" + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI
                 + "&response_type=" + RESPONSE_TYPE + "&scope=" + SCOPE;
     }

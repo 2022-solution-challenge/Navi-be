27dbc8e11d5306307fb87b7fe01849072732f1e5
grace_goose <choieungi@gm.gist.ac.kr>
2022-03-16 22:51:48 +0900

    feat: stomp config

---
+++ src/main/java/com/google/solution/config/StompWebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.google.solution.config;
+
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketMessagingAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+}
